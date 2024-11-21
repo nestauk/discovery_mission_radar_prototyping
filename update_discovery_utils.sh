e50f4921a1b2e4f14ddd8132729f81ea63db23ca
beingkk <kandersk@gmail.com>
2024-11-21 10:47:39 +0000

    fix: deduplication bug

--- update_discovery_utils.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sed -i.bak 's/^discovery-utils/#discovery-utils/' pyproject.toml && \
-poetry lock && \
-poetry install && \
-sed -i.bak 's/^#discovery-utils/discovery-utils/' pyproject.toml && \
-poetry lock && \
-poetry install && \
-rm pyproject.toml.bak
