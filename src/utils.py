7ff5cb4560f5fc4215e2473a7944cdb9b6f8e4fc
beingkk <kandersk@gmail.com>
2025-03-06 12:21:35 +0000

    AHL scan

---
+++ src/utils.py
@@ -0,0 +1,66 @@
+
+import pandas as pd
+
+from src import PROJECT_DIR, logging
+
+from discovery_utils.utils.io import safe_yaml_load
+from discovery_utils.utils.llm import batch_check
+
+
+PROJECT_NAME = "2025_02_MS_asf"
+OUTPUT_DIR = PROJECT_DIR / f"data/{PROJECT_NAME}"
+
+CONFIG_NAMES = [
+    "bioenergy",
+    "biomass_heating",
+    "built_environment",
+    "ccus",
+    "district_heating",
+    "energy_efficiency",
+    "energy_grid",
+    "geothermal_energy",
+    "green_skills",
+    "heat_pumps",
+    "heat_storage",
+    "hydrogen_energy",
+    "hydrogen_heating",
+    "micro_chp",
+    "solar_thermal",
+]
+
+CB_CATEGORIES = {
+    # "decarbonisation_general",
+    "energy_storage": "Battery",
+    "renewables_general": "Renewable Energy",
+    "solar": "Solar",
+    "wind": "Wind Energy"
+}
+
+def get_config_dict(config_name: str) -> dict:
+    """Find companies in a specific category from a config file"""
+    config_path = str(PROJECT_DIR / f"notebooks/{PROJECT_NAME}/config_{config_name}.yaml")
+    return safe_yaml_load(open(config_path))
+
+def get_companies_from_config(CB, config: dict) -> pd.DataFrame:
+    """Get companies from a config file"""
+    category_name = config["search_recipe"]["category_name"]
+    return CB.get_companies_in_nesta_categories("topic_labels", [category_name])
+
+async def check_relevance(CB, selected_df: pd.DataFrame, config_name: str, config: dict) -> None:
+    """Check relevance of the selected companies"""
+    selected_texts_df = CB.get_organisation_text(selected_df)
+    check_data = dict(zip(selected_texts_df['id'], selected_texts_df['text']))
+    system_message = batch_check.generate_relevance_check_system_message(config)
+
+    fields = [
+        {"name": "is_relevant", "type": "str", "description": "A one-word answer: 'yes' or 'no'."},
+    ]
+
+    processor = batch_check.LLMProcessor(
+        output_path=str(OUTPUT_DIR / f"llm_check_MS_{config_name}.jsonl"),
+        system_message=system_message,
+        session_name="mission_studio",
+        output_fields=fields,
+    )
+
+    await processor.run(check_data, batch_size=10, sleep_time=0.5)    
